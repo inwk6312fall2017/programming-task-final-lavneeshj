dc369afebca4d8dd8e71e89df935a3cb72293fbb
Lavneesh Jaggi <lavneeshjaggi@TCDB9.WPA.Dal.Ca>
2017-10-20 14:34:24 -0300

    code running partially, also weather's partially cloudy

---
+++ task2.py
@@ -0,0 +1,23 @@
+from weather import Weather
+weather = Weather()
+
+# Lookup WOEID via http://weather.yahoo.com.
+
+lookup = weather.lookup('4177')
+condition = lookup.condition()
+print(condition['text'])
+
+# Lookup via location name.
+
+location = weather.lookup_by_location('halifax')
+condition = location.condition()
+print(condition['text'])
+
+# Get weather forecasts for the upcoming days.
+
+forecasts = location.forecast()
+for forecast in forecasts:
+    print(forecasts.text())
+    print(forecasts.date())
+    print(forecasts.high())
+    print(forecasts.low())
