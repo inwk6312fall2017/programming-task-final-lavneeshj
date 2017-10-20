16fbcf2f8760c707f7a927748c81182856856244
Lavneesh Jaggi <lavneeshjaggi@TCDB9.WPA.Dal.Ca>
2017-10-20 14:11:56 -0300

    longest word in Book1.txt found

--- task1.py
+++ task1.py
@@ -1,25 +1,46 @@
-def longest_word(filename):
+#def longest_word(filename):
     
-    with open(filename) as f:
+#    with open(filename) as f:
         
-        for line in f:
-            words = line.split('.')
-            longest = ''
-            for word in words:
-                if len(longest) < len(word):
-                    longest = word
-            print("Line has", longest, "as the longest word.")
+#        for line in f:
+#            words = line.split('.')
+#            longest = ''
+#            for word in words:
+#                if len(longest) < len(word):
+#                   longest = word
+#            print("Line has", longest, "as the longest word.")
            
-            return(longest)
+ #           return(longest)
 
 
-filename1='Book1.txt'
-filename2='Book2.txt'
-filename3='Book3.txt'
+#filename1='Book1.txt'
+#filename2='Book2.txt'
+#filename3='Book3.txt'
 
-longest_word(filename1)
+#longest_word(filename1)
 
-longest_word(filename2)
+#longest_word(filename2)
 
-longest_word(filename3)
+#longest_word(filename3)
 
+
+def longest_word(file):
+
+	a= open(file,'r')
+	l = a.readlines()
+	string = l[0]
+	stringsplit = string.split()
+
+	a = []
+	for i in stringsplit:
+    		a.append(len(i))
+    		e = max(a)
+	for j in stringsplit:
+    		if len(j) == e:
+        		print("The longest word in", file,"is", j, "and it is", len(j),"characters long")
+
+file1 = 'Book1.txt'
+file2 = 'Book2.txt'
+file3 = 'Book3.txt'
+
+longest_word(file1)
