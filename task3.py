87aa0987fb61da6f87c1142e659e8c370ed409f5
Lavneesh Jaggi <lavneeshjaggi@TCDB9.WPA.Dal.Ca>
2017-10-20 15:26:29 -0300

    commit 1

---
+++ task3.py
@@ -0,0 +1,14 @@
+
+def func(file):
+
+	
+	with open(file) as d:
+		text = d.readlines()
+		for line in text:
+			words = line.split()
+			print(words.replace('172','192'))
+
+
+
+file1='running-config.cfg'
+func(file1)
